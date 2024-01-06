6b87f36f5b4e8764c6362f81d05069a8b8cabeca
jonasmark97 <jonasmar14@gmail.com>
2024-01-06 15:13:03 +0100

    updated make_dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,3 +1,15 @@
+import glob
+import os 
+import tqdm
+from PIL import Image
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))  # Change working directory to this one
+
 if __name__ == '__main__':
     # Get the data and process it
-    pass
+    list_of_raw_images = glob.glob(os.path.join("..","..","data","raw","*","*","*.jpg"))
+
+    for filename in tqdm.tqdm(list_of_raw_images):
+        img = Image.open(filename)
+        img.save(os.path.join("..","..","data","processed",os.path.basename(filename)))
+
