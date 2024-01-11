9a9f1fea0a2c9374edc02b8c196152e86393aa31
Amin <BlackError76@yahoo.com>
2024-01-11 23:50:13 +0100

    Updated api.py

--- src/api.py
+++ src/api.py
@@ -1,34 +1,38 @@
 import cv2
+import numpy as np
 import torch
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
 
 from src.models.model import MyNeuralNet
 
+
+classes = ["beagle", "bulldog", "dalmatian", "german-shepherd", 
+           "husky", "labrador-retriever", "poodle", "rottweiler"]
+
+
 app = FastAPI()
-model = MyNeuralNet()
-model = MyNeuralNet.load_from_checkpoint(checkpoint_path="model.ckpt")
+model = MyNeuralNet.load_from_checkpoint(lr=1e-4, checkpoint_path="models/model.ckpt")
 model.eval()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
-@app.post("/model/")
+@app.post("/model")
 async def cv_model(data: UploadFile = File(...)):
-    with open('image.jpg', 'wb') as image:
-        content = await data.read()
-        image.write(content)
-        img = cv2.imread("image.jpg")
-        res = cv2.resize(img, (64, 64)) # TODO: confirm this is actual model input shape
-        prediction = model(res)
-        image.close()
+    if not data.content_type.startswith("image/"):
+        return JSONResponse(content={"error": "Invalid file type. Only images are allowed."}, status_code=400)
+    
+    content = await data.read()
 
-    return prediction
+    # TODO: Instead, it's better to pass the image to predict_model and let it do the 
+    # resizing (and other preprocessing including normalization)
+    img = cv2.imdecode(np.fromstring(content, np.uint8), cv2.IMREAD_COLOR)
+    img = cv2.resize(img, (128, 128)) # TODO: confirm this is actual model input shape
+    img = torch.from_numpy(img).permute(2, 0, 1).unsqueeze(0).float()
+
+    probs = torch.softmax(model(img), dim=1)
+    prediction = classes[probs.argmax(dim=1).item()]
+    confidence = probs.max(dim=1).values.item()
+
+    return {"prediction": prediction, "confidence": confidence}
