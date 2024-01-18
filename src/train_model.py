44ca81cf475f84e246c5489b90bccd8bc87f53cb
jonasmark97 <jonasmar14@gmail.com>
2024-01-18 19:22:44 +0100

    Removed logger again

--- src/train_model.py
+++ src/train_model.py
@@ -3,8 +3,8 @@ import hydra
 import pytorch_lightning as pl
 import torch
 import torchvision.transforms as transforms
-from datetime import datetime
-import wandb
+# from datetime import datetime
+# import wandb
 
 from src.data_loader import get_data  
 from src.models.model import MyNeuralNet
@@ -16,22 +16,22 @@ os.environ["MIN_TEST_LOSS"] = str(100000000.0)
 @hydra.main(config_path=os.path.join(BASE_DIR, "src/conf"), config_name="training_config", version_base=None)
 def main(cfg):
 
-    wandb_logger = pl.loggers.WandbLogger(
-        name = wandb.api.viewer()["username"] + " - " + datetime.now().strftime("%Y-%m-%d %H:%M"), # run name format : username - timestamp 
-        log_model=False, # if log_model == 'all', checkpoints are logged during training.
-                        # if True, checkpoints are logged at the end of training, except when save_top_k == -1 which also logs every checkpoint during training.
-                        # if log_model == False (default), no checkpoint is logged.
-        project="Classification of dogbreeds",
-        config={
-            "architecture": "PretrainedVGG16",
-            "dataset": "8DogBreeds",
-            "learning_rate": cfg.lr,
-            "batch_size" : cfg.batch_size,
-            "max_epochs": cfg.max_epochs,
-            "validation_ratio" : cfg.validation_ratio,
-            "testing_ratio" : cfg.testing_ratio,
-            "transforms" : "target_shape:"+str(tuple(cfg.target_shape))
-        })
+    # wandb_logger = pl.loggers.WandbLogger(
+    #     name = wandb.api.viewer()["username"] + " - " + datetime.now().strftime("%Y-%m-%d %H:%M"), # run name format : username - timestamp 
+    #     log_model=False, # if log_model == 'all', checkpoints are logged during training.
+    #                     # if True, checkpoints are logged at the end of training, except when save_top_k == -1 which also logs every checkpoint during training.
+    #                     # if log_model == False (default), no checkpoint is logged.
+    #     project="Classification of dogbreeds",
+    #     config={
+    #         "architecture": "PretrainedVGG16",
+    #         "dataset": "8DogBreeds",
+    #         "learning_rate": cfg.lr,
+    #         "batch_size" : cfg.batch_size,
+    #         "max_epochs": cfg.max_epochs,
+    #         "validation_ratio" : cfg.validation_ratio,
+    #         "testing_ratio" : cfg.testing_ratio,
+    #         "transforms" : "target_shape:"+str(tuple(cfg.target_shape))
+    #     })
 
     transform = transforms.Compose([
             transforms.ToTensor(),
@@ -51,8 +51,8 @@ def main(cfg):
     train_loader, valid_loader, test_loader = get_data(cfg.batch_size, cfg.validation_ratio, cfg.testing_ratio, transform=transform)
 
     # Setup trainer
-    trainer = pl.Trainer(logger=wandb_logger, log_every_n_steps=1, devices=1, accelerator=accelerator, max_epochs=cfg.max_epochs)
-    # trainer = pl.Trainer(log_every_n_steps=1, devices=1, accelerator=accelerator, max_epochs=cfg.max_epochs)
+    # trainer = pl.Trainer(logger=wandb_logger, log_every_n_steps=1, devices=1, accelerator=accelerator, max_epochs=cfg.max_epochs)
+    trainer = pl.Trainer(log_every_n_steps=1, devices=1, accelerator=accelerator, max_epochs=cfg.max_epochs)
 
     # Train
     trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=valid_loader)
